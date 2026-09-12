244858c4d983d2f3d401d0524fafba378712738d
Md gulam Noxboundy <gulamnoxbondy@gmail.com>
2026-09-13 00:51:23 +0200

    Add new allowed origin to CORS configuration

--- src/main/java/se/lexicon/skalmansfoodsleepclock/config/CorsConfig.java
+++ src/main/java/se/lexicon/skalmansfoodsleepclock/config/CorsConfig.java
@@ -6,12 +6,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Override
+   @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
                         "http://localhost:5173",
-                        "http://10.218.120.138:5173"
+                        "http://10.218.120.138:5173",
+                        "https://www.olduhr.de"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
